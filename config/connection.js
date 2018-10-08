4da7a19b0a354a9a73f93b419105cb080a056cca
Nikee <rivera.nikee@gmail.com>
2018-10-07 22:13:13 -0400

    added to conenction.js

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "cat_db"
-});
+// Added for Heroku.
+if (process.env.burger_db_url) {
+  connection = mysql.createConnection(process.env.burger_db_url);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'burger_db'
+  });
+};
 
 // Make connection.
 connection.connect(function(err) {
