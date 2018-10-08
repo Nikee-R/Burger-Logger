850a1d1c3d35d598d7e62141ef9f6d57dfbf65d8
Nikee <rivera.nikee@gmail.com>
2018-10-08 00:25:13 -0400

    Changes.

--- models/burger.js
+++ models/burger.js
@@ -9,21 +9,16 @@ var burger = {
   },
   // The variables cols and vals are arrays.
   create: function(vals, cb) {
-    orm.create("burgers", "burger_name", vals, function(res) {
+    orm.create("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(condition, cb) {
-    orm.update("burgers", condition, function(res) {
+    orm.update("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   },
-  delete: function(condition, cb) {
-    orm.delete("cats", condition, function(res) {
-      cb(res);
-    });
-  }
 };
 
 // Export the database functions for the controller (catsController.js).
-module.exports = burgers;
+module.exports = burger;
