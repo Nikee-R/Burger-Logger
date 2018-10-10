1128ba88c36ecf2607ac13a2f3d1f7b9ec6fedbc
Nikee <rivera.nikee@gmail.com>
2018-10-09 22:49:08 -0400

    Changes to connection.js and package.

--- config/connection.js
+++ config/connection.js
@@ -3,14 +3,14 @@ var mysql = require("mysql");
 var connection;
 
 // Added for Heroku.
-if (process.env.burger_db_url) {
-  connection = mysql.createConnection(process.env.burger_db_url);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    host: 'l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-    user: 'lfj0ox2udfu0j46m',
-    password: 'kurdtobiz3fbobta',
-    database: 'n82s8uriig2iq71f'
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'burger_db'
   });
 };
 
