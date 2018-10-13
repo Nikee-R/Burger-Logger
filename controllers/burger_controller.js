f144556d8f5a9ecb417dafaabd7c90cdc69c83e7
Nikee <rivera.nikee@gmail.com>
2018-10-13 10:36:28 -0400

    fixed error

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -15,13 +15,13 @@ router.get("/", function(req, res) {
 });
 
 router.put("/burgers/update", function(req, res) {
-  burger,update(req.body.burger_id, function(result) {
+  burger.update(req.body.burger_id, function(result) {
     console.log(result);
     res.redirect('/');
   });
 });
 
-router.post('/burgers.create', function(req, res) {
+router.post('/burgers/create', function(req, res) {
   burger.create(req.body.burger_name, function(result) {
     res.redirect('/');
   })
