c44dd133dc5cadf3b8504d93497cf6e05b68c857
Nikee <rivera.nikee@gmail.com>
2018-10-14 00:35:42 -0400

    Fixed error in orm.

--- config/orm.js
+++ config/orm.js
@@ -10,11 +10,11 @@ var orm = {
         throw err;
       }
       cb(result)
-    })
+    });
   },
 
   update: function(tableInput, condition, cb) {
-    connection.query('UPDATE ' + tableInput + 'SET devoured=true WHERE id' + condition + ';', function(err, result) {
+    connection.query('UPDATE ' + tableInput + 'SET devoured=true WHERE id=' + condition + ';', function(err, result) {
       if (err) {
         throw err;
       }
